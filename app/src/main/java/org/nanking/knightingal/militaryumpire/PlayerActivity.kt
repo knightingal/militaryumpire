5c8e09533e131d2c5ce2878f7568abddc78ef23d
knightingal <yujianming1985@hotmail.com>
2025-01-27 17:45:32 +0800

    register for activityResult

--- app/src/main/java/org/nanking/knightingal/militaryumpire/PlayerActivity.kt
+++ app/src/main/java/org/nanking/knightingal/militaryumpire/PlayerActivity.kt
@@ -1,15 +1,40 @@
 package org.nanking.knightingal.militaryumpire
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import org.nanking.knightingal.militaryumpire.databinding.ActivityPlayerBinding
 
 class PlayerActivity: AppCompatActivity() {
+
+    private var getContent = registerForActivityResult(OCRParse()) { it ->
+        Log.d("PlayerActivity", "ocr result $it")
+    }
 
     private lateinit var viewBinding: ActivityPlayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+        viewBinding.player1.setOnClickListener {
+            getContent.launch(null)
+        }
     }
 }
+
+class OCRParse : ActivityResultContract<Void?, String?>() {
+    override fun createIntent(context: Context, input: Void?) =
+        Intent(context, MainActivity::class.java)
+
+    override fun parseResult(resultCode: Int, intent: Intent?) : String? {
+        if (resultCode != Activity.RESULT_OK) {
+            return null
+        }
+        return intent?.getStringExtra("ocr")
+    }
+}
+
