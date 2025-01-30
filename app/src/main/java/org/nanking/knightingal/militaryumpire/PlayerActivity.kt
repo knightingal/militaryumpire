6bd7930ea4711889c347dc65e4b2193c6d915586
knightingal <yujianming1985@hotmail.com>
2025-01-30 18:09:57 +0800

    make `registerForActivityResult` simple

--- app/src/main/java/org/nanking/knightingal/militaryumpire/PlayerActivity.kt
+++ app/src/main/java/org/nanking/knightingal/militaryumpire/PlayerActivity.kt
@@ -1,29 +1,16 @@
 package org.nanking.knightingal.militaryumpire
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import org.nanking.knightingal.militaryumpire.databinding.ActivityPlayerBinding
 
 class PlayerActivity: AppCompatActivity() {
 
-    private var activityResult = registerForActivityResult(object : ActivityResultContract<Void?, String?>() {
-
-        override fun createIntent(context: Context, input: Void?) =
-            Intent(context, ResultActivity::class.java)
-
-        override fun parseResult(resultCode: Int, intent: Intent?) : String? {
-            if (resultCode != Activity.RESULT_OK) {
-                return null
-            }
-            return intent?.getStringExtra("ocr")
-        }
-    }) { it ->
-        Log.d("PlayerActivity", "ocr result $it")
+    private var activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        Log.d("PlayerActivity", "ocr result ${it.data!!.getStringExtra("ocr")}")
     }
 
     private lateinit var viewBinding: ActivityPlayerBinding
@@ -32,7 +19,7 @@ class PlayerActivity: AppCompatActivity() {
         viewBinding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         viewBinding.player1.setOnClickListener {
-            activityResult.launch(null)
+            activityResult.launch(Intent(this, ResultActivity::class.java))
         }
     }
 
